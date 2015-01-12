4734f0e1879afac75b0f500afce9b855d023bbc6
Bret Shawn <bret.shawn@gmail.com>
2015-01-12 02:47:22 +0000

    Add web service methods

--- app/video-player.js
+++ app/video-player.js
@@ -1,28 +1,108 @@
+var Video = (function () {
+   function Video(id, name, src) {
+       this.id = id;
+       this.name = name;
+       this.src = src;
+   }
+   
+   return Video;
+} ());
+
+var VideoService = (function () {
+    "use strict";
+    
+    function VideoService () {
+        this.url = 'api/videos';
+    }
+    
+    VideoService.prototype.getAll = function() {
+        var dfd = $.Deferred();
+        
+        $.get(this.url)
+            .done(getAll_done.bind(this, dfd))
+            .fail(console.log);
+        
+        return dfd.promise();
+    };
+    
+    VideoService.prototype.get = function(videoId) {
+        var dfd = $.Deferred();
+        
+        $.get(this.url + '/' + videoId)
+            .done(get_done.bind(this, dfd))
+            .fail(console.log);
+        
+        return dfd.promise();
+    };
+    
+    function getAll_done(dfd, results) {
+        var videos = results.map(function (video) {
+            return new Video(video.id, video.name);
+        });
+        
+        dfd.resolve(videos);
+    }
+    
+    function get_done(dfd, result) {
+        dfd.resolve(new Video(result.id, result.name, result.src));
+    }
+    
+    return VideoService;
+} ());
+
+
 var ViewModel = (function () {
     "use strict";
 
-    function ViewModel (){
+    function ViewModel () {
+        this.service = new VideoService();
+        
         this.applicationTitle = ko.observable('Video Player');
-        this.videos = ko.observableArray([
-            { name: 'Big Buck Bunny', src: 'movies/big_buck_bunny.mp4' },
-            { name: 'Lions', src: 'movies/Die leeutemmer.mp4' },
-            { name: 'Incredible Mara Leopard Attack', src: 'movies/Incredible Mara Leopard Attack21.mp4' }
-        ]);
-        this.selectedVideo = ko.observable(this.videos()[0]);
+        this.videos = ko.observableArray();
+        this.selectedVideo = ko.observable();
     }
+    
+    ViewModel.prototype.initialize = function initialize() {
+        loadVideos.call(this);
+    };
 
     ViewModel.prototype.isSelected = function isSelected(video) {
-       return video === this.selectedVideo();
+        if(!this.selectedVideo()) {
+            return false;
+        }
+        return video.id === this.selectedVideo().id;
     };
 
     ViewModel.prototype.select = function select(video) {
-        this.selectedVideo(video);
+        loadVideoDetails.call(this, video.id);
     };
+    
+    function loadVideos() {
+        this.service.getAll()
+            .done(getAll_done.bind(this))
+            .fail(console.log);
+    }
+    
+    function getAll_done(videos) {
+        this.videos(videos);
+        this.select(this.videos()[0]);
+    }
+    
+    function loadVideoDetails(videoId) {
+        this.service.get(videoId)
+            .done(get_done.bind(this))
+            .fail(console.log);
+    }
+    
+    function get_done(video) {
+        this.selectedVideo(video);
+    }
 
     return ViewModel;
-}());
+} ());
 
 $(function () {
     var vm = new ViewModel();
+    vm.initialize();
     ko.applyBindings(vm);
 });
--- server/configure-routes.js
+++ server/configure-routes.js
@@ -2,6 +2,39 @@ function configureRoutes(app) {
     app.get('/', function(req, res) {
        res.sendFile('index.html', { root: '../' });
     });
+    
+    app.get('/api/videos', function(req, res) {
+        // Load data.
+        var data = loadVideoList();
+        res.send(data);
+    });
+    
+    app.get('/api/videos/:id', function(req, res, next) {
+        var id = parseInt(req.params.id);
+        var data = loadVideoDetails(id);
+        res.send(data);
+    });
+}
+
+function loadVideoList() {
+    // Loads summary data for all videos in our list.
+    return [
+        { id: 1, name: 'Big Buck Bunny' },
+        { id: 2, name: 'Lions' },
+        { id: 3, name: 'Incredible Mara Leopard Attack' }
+    ];
+}
+
+function loadVideoDetails(videoId) {
+    // Loads detailed data for a particular video.
+    switch(videoId) {
+        case 1:
+            return { id: 1, name: 'Big Buck Bunny', src: 'videos/big_buck_bunny.mp4' };
+        case 2:
+            return { id: 2, name: 'Lions', src: 'videos/Die leeutemmer.mp4' };
+        case 3:
+            return { id: 3, name: 'Incredible Mara Leopard Attack', src: 'videos/Incredible Mara Leopard Attack21.mp4' };
+    }
 }
 
 module.exports = configureRoutes;
