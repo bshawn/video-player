dcc3fe45ea07bc951648b63faf3954b94bcfa134
bshawn <bret.shawn@gmail.com>
2015-02-24 20:31:07 -0500

    Begin implementing socket.io

--- server/server.js
+++ server/server.js
@@ -4,26 +4,27 @@
 /*global console*/
 /*global __dirname*/
 
-var path = require('path');
 var express = require('express');
 var app = express();
+var socketio = require('socket.io');
+
+// Serve the client directory as static files.
+app.use(express.static('client/default'));
+app.use('/remote', express.static('client/remote'));
+app.use('/viewer', express.static('client/viewer'));
 
 // Configure routes.
 require('./configure-routes')(app);
-
-// Serve the client directory as static files.
-app.use(express.static('client'));
-
-// // Handle 404s.
-// app.use(function(req, res, next) {
-//    res.status(404).send('Unable to locate requested resource');
-//
-//    // By not calling next(), we are terminating the pipeline.
-// });
 
 var server = app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', function() {
     var host = server.address().address,
         port = server.address().port;
 
     console.log('Server listening at http://%s:%s', host, port);
+});
+
+// Socket IO for separating the remote from the viewer.
+var io = socketio.listen(server);
+io.on('connection', function(socket) {
+    console.log('connection made');
 });
